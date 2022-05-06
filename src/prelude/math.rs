f05096a4029555edaca2c4bc74e9234289808897
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-05-06 13:38:39 +0200

    Add more math

--- src/prelude/math.rs
+++ src/prelude/math.rs
@@ -3,12 +3,12 @@
 //!
 
 pub use cgmath::{
-    dot, frustum, ortho, perspective, vec2, vec3, vec4, Deg, Matrix2, Matrix3, Matrix4, Quaternion,
-    Rad, Vector2, Vector3, Vector4,
+    dot, frustum, ortho, perspective, vec2, vec3, vec4, Deg, Matrix2, Matrix3, Matrix4, Point2,
+    Point3, Quaternion, Rad, Vector2, Vector3, Vector4,
 };
 pub use cgmath::{
-    Angle, InnerSpace, Matrix, MetricSpace, One, Rotation, Rotation2, Rotation3, SquareMatrix,
-    Transform, Transform2, Transform3, VectorSpace, Zero,
+    Angle, EuclideanSpace, InnerSpace, Matrix, MetricSpace, One, Rotation, Rotation2, Rotation3,
+    SquareMatrix, Transform, Transform2, Transform3, VectorSpace, Zero,
 };
 
 pub type Vec2 = Vector2<f32>;
@@ -18,10 +18,18 @@ pub type Mat2 = Matrix2<f32>;
 pub type Mat3 = Matrix3<f32>;
 pub type Mat4 = Matrix4<f32>;
 pub type Quat = Quaternion<f32>;
+pub type Degrees = Deg<f32>;
+pub type Radians = Rad<f32>;
 
 pub const fn degrees<T>(v: T) -> Deg<T> {
     cgmath::Deg(v)
 }
 pub const fn radians<T>(v: T) -> Rad<T> {
     cgmath::Rad(v)
+}
+
+pub fn rotation_matrix_from_dir_to_dir(source_dir: Vec3, target_dir: Vec3) -> Mat4 {
+    Mat4::from(Mat3::from(cgmath::Basis3::between_vectors(
+        source_dir, target_dir,
+    )))
 }
