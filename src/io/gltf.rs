6005df2ff7980985014f6aa0391ca322928430ad
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-11-11 13:09:00 +0100

    Revert "A bit more clean-up"
    This reverts commit 9a12ba57c4d41749ad1ef0577534e803501b2f93.

--- src/io/gltf.rs
+++ src/io/gltf.rs
@@ -64,16 +64,18 @@ pub fn deserialize_gltf(raw_assets: &mut RawAssets, path: &PathBuf) -> Result<Mo
         buffers.push(::gltf::buffer::Data(data));
     }
 
-    for node in document.nodes() {
-        parse_tree(
-            &Mat4::identity(),
-            &node,
-            raw_assets,
-            &base_path,
-            &buffers,
-            &mut cpu_meshes,
-            &mut cpu_materials,
-        )?;
+    for scene in document.scenes() {
+        for node in scene.nodes() {
+            parse_tree(
+                &Mat4::identity(),
+                &node,
+                raw_assets,
+                &base_path,
+                &buffers,
+                &mut cpu_meshes,
+                &mut cpu_materials,
+            )?;
+        }
     }
     Ok(Model {
         geometries: cpu_meshes,
